d4f502ff90296b23bcb5820b18a6cc63ee62e658
girardpierre <girardpierre79@hotmail.fr>
2016-10-26 16:19:27 +0200

    ! img header

--- js/app.js
+++ js/app.js
@@ -1,94 +1,104 @@
-  (function(){
-    var app = angular.module('plusPresDeSoi', ["ngRoute"], function($locationProvider){
-    });
+(function(){
+  var app = angular.module('plusPresDeSoi', ["ngRoute"], function($locationProvider){
+  });
 
-    app.config(['$routeProvider',function($routeProvider){
-      $routeProvider
-      .when('/',{
-        templateUrl : 'partials/home/home.html',
-        controller : 'imageHeader'
-      })
-      .when('/apropos',{
-        templateUrl : 'partials/apropos/apropos.html',
-        controller : 'imageHeader'
-      })
-      .when('/atelier',{
-        templateUrl : 'partials/atelier/atelier.html'
-      })
-      .when('/contact',{
-        templateUrl : 'partials/contact/contact.html'
-      })
-      .when('/dietetique',{
-        templateUrl : 'partials/dietetique/dietetique.html'
-      })
-      .when('/magnetisme',{
-        templateUrl : 'partials/magnetisme/magnetisme.html'
-      })
-      .when('/mention',{
-        templateUrl : 'partials/mention/mention.html'
-      })
-    }
-  ]);
+  app.config(['$routeProvider',function($routeProvider){
+    $routeProvider
+    .when('/',{
+      templateUrl : 'partials/home/home.html'
+    })
+    .when('/apropos',{
+      templateUrl : 'partials/apropos/apropos.html'
+    })
+    .when('/atelier',{
+      templateUrl : 'partials/atelier/atelier.html'
+    })
+    .when('/contact',{
+      templateUrl : 'partials/contact/contact.html'
+    })
+    .when('/dietetique',{
+      templateUrl : 'partials/dietetique/dietetique.html'
+    })
+    .when('/magnetisme',{
+      templateUrl : 'partials/magnetisme/magnetisme.html'
+    })
+    .when('/mention',{
+      templateUrl : 'partials/mention/mention.html'
+    })
+  }]);
 
-    app.controller('imageHeader', function($location){
-      this.url = location.path;
-      this.page = null;
-      this.texte = null;
-      switch (this.url) {
+  app.controller('imageHeader', function($location){
+    this.url = $location.path();
+    this.page = "pg-home";
+    this.texte1 = "Comment je m'aime ?";
+    this.texte2 = "Comment je mange ?";
+
+    this.changeImg = function (url){
+      switch (url) {
         case "/":
+            $location.path('');
             this.page = "pg-home";
-            this.texte = "<h1>Comment je m'aime ?</h1><h1 id=\"decalagePhraseAccroche\">Comment je mange ?</h1>";
-          break;
-            case "/dietetique":
-                this.page = "pg-dietetique";
-              break;
-              case "/magnetisme":
-                  this.page = "pg-magnetisme";
-                break;
-                case "/apropos":
-                    this.page = "pg-apropos";
-                    this.texte = "<h1>Qui suis-je ?</h1>";
-                  break;
-                  case "/contact":
-                      this.page = "pg-contact";
-                    break;
-
+            this.texte1 = "Comment je m'aime ?";
+            this.texte2 = "Comment je mange ?";
+            break;
+        case "/dietetique":
+            $location.path('dietetique');
+            this.page = "pg-dietetique";
+            this.texte1 = "";
+            this.texte2 = "";
+            break;
+        case "/magnetisme":
+            $location.path('magnetisme');
+            this.page = "pg-magnetisme";
+            this.texte1 = "";
+            this.texte2 = "";
+            break;
+        case "/atelier":
+            $location.path('atelier');
+            this.page = "pg-atelier";
+            this.texte1 = "";
+            this.texte2 = "";
+            break;
+        case "/apropos":
+            $location.path('apropos');
+            this.page = "pg-apropos";
+            this.texte1 = "Qui suis-je ?";
+            this.texte2 = "";
+            break;
+        case "/contact":
+            $location.path('contact');
+            this.page = "pg-contact";
+            this.texte1 = "";
+            this.texte2 = "";
+            break;
+        case "/mention":
+            $location.path('mention');
+            this.page = "pg-apropos";
+            this.texte1 = "";
+            this.texte2 = "";
+            break;
         default:
-          this.page = "pg-home";
-      }
-    });
+            $location.path('');
+            this.page = "pg-home";
+            this.texte1 = "Comment je m'aime ?";
+            this.texte2 = "Comment je mange ?";
+      };
+    }
 
-    app.directive('header',function(){
-      return{
-        restrict : 'A',
-        templateUrl : 'partials/common/header.html'
-      }
-    });
+  });
 
-    app.directive('footer',function(){
-      return{
-        restrict : 'A',
-        templateUrl : 'partials/common/footer.html'
-      }
-    });
+  app.directive('header',function(){
+    return{
+      restrict : 'A',
+      templateUrl : 'partials/common/header.html'
+    }
+  });
 
-      /*var data = [
-          apropos = [
-            bio = [
-              bioTexte : "",
-              bioImage :
-            ],
-            formation : [
+  app.directive('footer',function(){
+    return{
+      restrict : 'A',
+      templateUrl : 'partials/common/footer.html'
+    }
+  });
 
-            ],
-            presse : [
-
-            ],
-            temoignage : [
-
-            ]
-
-          ];
-      ];*/
-
-  })();
+})();
--- js/vendor/script.js
+++ js/vendor/script.js
@@ -1,29 +1,14 @@
 $( function() {
-  /* dietetique - animation de conseil et recette*/
-  $('#recette a').click(function (e) {
-    e.preventDefault()
-    $(this).tab('show')
-  });
-  $('#conseil a').click(function (e) {
-    e.preventDefault()
-    $(this).tab('show')
-  });
-  $('.tabDietetique').click(function() {
-    $('.tabDietetique').removeClass('activeLien');
-    $(this).addClass('activeLien');
-  });
+
   // changer la couleur de typo sur l'onglet actif de recette et conseil
   $('.tabDietetique').click(function() {
     $('.tabDietetique').removeClass('activeLien');
     $(this).addClass('activeLien');
   });
-  // changement de la classe active des onglets recette et conseil lors des choix de la page home
-  /*$('#boutonHomeConseil').click(function() {
-    $('#recette').removeClass('active');
-    $('#conseil').addClass('active');
-  });*/
-  $('.required-icon').tooltip({
+  // changement d'onglet recette-conseil lors du choix sur la page home
+
+  /*$('.required-icon').tooltip({
     placement: 'left',
     title: 'Required field'
-  });
+  });*/
 });
